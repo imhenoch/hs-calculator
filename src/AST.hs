e5353fe41c8eccd88033ebd8bb81054066f2ac4b
Isidro Henoch <isidro.henoch@gmail.com>
2019-03-15 08:02:47 -0600

    Adds abstract syntax tree type class, it only supports addition, substraction, multiplication and division for now. Also, only works for integers.

---
+++ src/AST.hs
@@ -0,0 +1,10 @@
+-- Abstract Syntax Tree
+
+module AST where
+
+data Expression =
+      ExpAdd Expression Expression
+    | ExpSub Expression Expression
+    | ExpMul Expression Expression
+    | ExpDiv Expression Expression
+    | ExpInt Integer
