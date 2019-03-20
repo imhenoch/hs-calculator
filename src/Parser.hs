082df060e91a851906c88d1edec8a1f693abe7c4
Isidro Henoch <isidro.henoch@gmail.com>
2019-03-20 07:31:05 -0600

    Adds the parser, for now only has support for basic operations and only integers

---
+++ src/Parser.hs
@@ -0,0 +1,42 @@
+module Parser where
+
+import           AST
+import           Control.Monad (void)
+import           Text.Megaparsec
+import           Text.Megaparsec.Char
+import           Text.Megaparsec.Expr
+import qualified Text.Megaparsec.Lexer as L
+import           Text.Megaparsec.String (Parser)
+
+sc :: Parser ()
+sc = L.space (void spaceChar) lineComment blockComment
+ where
+  lineComment  = L.skipLineComment "//"
+  blockComment = L.skipBlockComment "/*" "*/"
+
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme sc
+
+symbol :: String -> Parser String
+symbol = L.symbol sc
+
+parentheses :: Parser a -> Parser a
+parentheses = between (symbol "(") (symbol ")")
+
+integer :: Parser Integer
+integer = lexeme L.decimal
+
+term = parentheses expr <|> ExpInt <$> integer <?> "term"
+
+table =
+  [ [binary "*" ExpMul, binary "/" ExpDiv]
+  , [binary "+" ExpAdd, binary "-" ExpSub]
+  ]
+
+binary name f = InfixL (symbol name >> return f)
+
+expr :: Parser Expression
+expr = makeExprParser term table <?> "expression"
+
+parseExpression :: String -> Either ParseError Expression
+parseExpression = parse expr ""
